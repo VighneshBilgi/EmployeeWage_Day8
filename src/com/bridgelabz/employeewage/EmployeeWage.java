5e095b31866c538bdbaf6e53694562b571197fac
VighneshBilgi <vhbilgi@gmail.com>
2022-05-02 15:17:28 +0530

    Added: UC7_Class_Method

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,20 +1,19 @@
 package com.bridgelabz.employeewage;
 
-public class EmployeeWage {
-
+class CalculateEmployeeWage{
     static final int WAGE_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
     static final int PART_DAY_HOUR = 4;
     static final int TOTAL_WORK_DAY = 20;
     static final int TOTAL_WORK_HOUR = 100;
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
+    static int dayCount = 1;
+    static int hourCount = 0;
+    static int totalWage = 0;
+    static int dayPresent = 0;
 
-        int dayCount = 1;
-        int hourCount = 0;
-        int totalWage = 0;
-        int dayPresent = 0;
+    public static void totalWage(){
+
         while (dayCount<=TOTAL_WORK_DAY && hourCount<=TOTAL_WORK_HOUR) {
 
             int dailyWage = 0;
@@ -45,5 +44,18 @@ public class EmployeeWage {
                 +dayPresent+ " days and \n"
                 + hourCount + " hours and \n"+
                 "earned total wages of "+ totalWage);
+
+    }
+}
+
+
+public class EmployeeWage {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+
+        CalculateEmployeeWage.totalWage();
+
     }
 }
